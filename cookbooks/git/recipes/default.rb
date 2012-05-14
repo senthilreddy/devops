de137674b38355f9c277b3634e42357c9b91a8b0
root <root@workstation01.thoughtworks.com>
2012-05-14 10:34:32 +0530

    added git

---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+bash "install_gitolite" do
+        user "root"
+        cwd "/tmp"
+        code <<-EOH
+        git clone git://github.com/sitaramc/gitolite.git
+        gitolite/install -ln /usr/bin
+        EOH
+end
+
